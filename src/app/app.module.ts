f478fc52b4e2a11e513e4b3aa901715e3b91024a
Francisco Bravo <fcobravo.23@gmail.com>
2024-10-16 20:46:12 -0300

    se agrego modulo gift y shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { GiftsModule } from './gifts/gifts.module';
+
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GiftsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
