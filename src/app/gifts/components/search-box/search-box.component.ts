1551ee83ba22c6d4b271eb3962a11eb414c7398d
Francisco Bravo <fcobravo.23@gmail.com>
2024-10-17 19:48:54 -0300

    se agrego estilos

--- src/app/gifts/components/search-box/search-box.component.ts
+++ src/app/gifts/components/search-box/search-box.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'gift-search-box',
     template: `<h5>Buscar</h5>
-    <input type="text" class="form-control" placeholder="Buscar Gift...">
+    <input type="text" class="form-control" placeholder="Buscar Gift..." style="margin-left: 15px;">
     `
 })
 export class SearchBoxComponent {
